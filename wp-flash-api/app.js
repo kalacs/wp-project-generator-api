b5c11483a57f26f1b48dbd2f916bb4bb433c279b
Szabó Ádám <szabo.adam@e-vista.hu>
2020-03-02 11:14:43 +0100

    Added cors config with origin  whitelist.

--- wp-flash-api/app.js
+++ wp-flash-api/app.js
@@ -1,29 +1,43 @@
-'use strict'
+'use strict';
 
-const path = require('path')
-const AutoLoad = require('fastify-autoload')
+const path = require('path');
+const AutoLoad = require('fastify-autoload');
+const cors = require('cors');
 
-module.exports = function (fastify, opts, next) {
+var whitelist = ['app://postman', 'http://localhost:4128'];
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+module.exports = function(fastify, opts, next) {
   // Place here your custom code!
   fastify.ready(() => {
-    console.log(fastify.printRoutes())
-  })
+    console.log(fastify.printRoutes());
+  });
+
+  fastify.use(cors(corsOptions));
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in services
   // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'services'),
-    options: Object.assign({}, opts)
-  })
+    options: Object.assign({}, opts),
+  });
 
   // Make sure to call next when done
-  next()
-}
+  next();
+};
